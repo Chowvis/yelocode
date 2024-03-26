7d0bd06e56141d2e88fcbac1b2cdc902320a540b
Chowvis <chvishallongchat98@gmail.com>
2024-03-26 14:49:56 +0530

    file upload

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -5,10 +5,13 @@ namespace App\Livewire;
 use App\Models\User;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
+
+use Livewire\WithFileUploads;
 use Livewire\WithPagination;
 
 class Clicker extends Component
 {
+    use WithFileUploads;
     use WithPagination;
     #[Rule('required|min:2|max:20')]
     public $name;
@@ -16,6 +19,8 @@ class Clicker extends Component
     public $email;
     #[Rule('required|min:5')]
     public $password;
+    #[Rule('nullable|sometimes|image|max:1024')]
+    public $image;
     public function render()
     {
         $users = User::paginate(5);
@@ -24,13 +29,15 @@ class Clicker extends Component
         ]);
     }
     public function createUser(){
+
         $validate = $this->validate();
+        if($this->image){
+            $validate['image'] = $this->image->store('uploads','public');
+        }
         User::create([
-            'name' => $validate['name'],
-            'email' => $validate['email'],
-            'password' => $validate['password'],
+            $validate,
         ]);
         $this->reset(['name','email','password']);
-        request()->session()->flash('success','User has been created');
+        session()->flash('success','User has been created');
     }
 }
