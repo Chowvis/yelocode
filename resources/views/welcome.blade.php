a6e6206f9aa32dad156d9ee4715ed0ee3a8d5ec0
Chowvis <chvishallongchat98@gmail.com>
2024-04-05 15:57:39 +0530

    ads

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
-        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.7/dist/cdn.min.js"></script>
+
 
     </head>
     <body class="antialiased">
